15918e9a1b0b95851c04b384d2f6278c63c392c3
Rohit <rrgawanderohit@gmail.com>
2025-05-21 16:15:45 +0530

    navbar completed

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+import Navbar from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/hero-section";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>  
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        <HeroSection/>
+      </div>
+    </div>
+  );
+}
